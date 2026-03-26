e41e5631439cbe7607baddf495526ea0700f4ef5
Md Kamrul Amin <kamjony7@gmail.com>
2026-03-26 12:33:10 -0400

    Master dataset path rename

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 FEATURE_COL = "hourly_demand"
 SEQUENCE_LENGTH = 24
-MASTER_DATASET_PATH = "outputs/master_dataset_2019_2023.csv"
+MASTER_DATASET_PATH = "outputs/master_dataset_2018_2023.csv"
 ARTIFACTS_SAVE_PATH = "outputs/processed_artifacts"
